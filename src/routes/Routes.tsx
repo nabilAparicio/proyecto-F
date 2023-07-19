623c0063e30a8eecefed6dd4cfb009756075816b
Orlando Aparicio <orlando.nabil@gmail.com>
2023-07-18 19:56:08 -0500

    inicializacion de app y navegacion basica

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Cuestionarios from "../screens/cuestionarios/Cuestionarios";
+import Home from "../screens/home/Home";
+
+export default function Routes() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Tab"
+        component={TabNavigator}
+      />
+      <Stack.Screen name="Cuestionarios" component={Cuestionarios} />
+    </Stack.Navigator>
+  );
+}
+
+const TabNavigator = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Home1" component={Home} />
+      <Tab.Screen name="Home2" component={Home} />
+      <Tab.Screen name="Home3" component={Home} />
+    </Tab.Navigator>
+  );
+};
---
+++ src/screens/cuestionarios/Cuestionarios.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Cuestionarios() {
+  return (
+    <View>
+      <Text>Cuestionarios</Text>
+    </View>
+  );
+}
